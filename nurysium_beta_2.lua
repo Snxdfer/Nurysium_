631b0f7635b069428bd6e831bbe0ad702aa17b42
Snxdfer <121313122+Snxdfer@users.noreply.github.com>
2024-11-16 22:34:04 -0300

    Update nurysium_beta_2.lua

--- nurysium_beta_2.lua
+++ nurysium_beta_2.lua
@@ -14,39 +14,6 @@ local deviceType = game:GetService("UserInputService"):GetPlatform() == Enum.Pla
 local ClientID = game:GetService("RbxAnalyticsService"):GetClientId()
 local FOV = game.Workspace.CurrentCamera.FieldOfView
 
-repeat task.wait(0.25) until game:IsLoaded()
-getgenv().Image = "rbxassetid://10734966248"
-getgenv().ToggleUI = "RightAlt"
-
-task.spawn(function()
-    if not getgenv().LoadedMobileUI then
-        getgenv().LoadedMobileUI = true
-        local OpenUI = Instance.new("ScreenGui")
-        local ImageButton = Instance.new("ImageButton")
-        local UICorner = Instance.new("UICorner")
-
-        OpenUI.Name = "OpenUI"
-        OpenUI.Parent = game:GetService("CoreGui")
-        OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-        ImageButton.Parent = OpenUI
-        ImageButton.BackgroundColor3 = Color3.fromRGB(105, 105, 105)
-        ImageButton.BackgroundTransparency = 0.8
-        ImageButton.Position = UDim2.new(0.9, 0, 0.1, 0)
-        ImageButton.Size = UDim2.new(0, 50, 0, 50)
-        ImageButton.Image = getgenv().Image
-        ImageButton.Draggable = true
-        ImageButton.Transparency = 1
-
-        UICorner.CornerRadius = UDim.new(0, 200)
-        UICorner.Parent = ImageButton
-
-        ImageButton.MouseButton1Click:Connect(function()
-            game:GetService("VirtualInputManager"):SendKeyEvent(true, getgenv().ToggleUI, false, game)
-        end)
-    end
-end)
-
 local RunService = game:GetService("RunService")
 local TweenService = game:GetService("TweenService")
 local Workspace = game:GetService("Workspace")
